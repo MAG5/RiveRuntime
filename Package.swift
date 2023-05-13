fdaa45b6208fc4401b1febdb66987900bd444cb3
Bin Xin <rhyzix@gmail.com>
2023-05-13 22:12:25 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,10 +9,9 @@ let package = Package(
             name: "RiveRuntime",
             targets: ["RiveRuntime"])],
     targets: [
-        .binaryTarget(
+        .target(
             name: "RiveRuntime",
-            url: "https://github.com/rive-app/rive-ios/releases/download/3.1.12/RiveRuntime.xcframework.zip",
-            checksum: "9665449ceb82a24afaef55b8d88167251fb2175c78c7e8eda2bdeb14461d611d"
+            path: "Source"
         ),
     ]
 )
